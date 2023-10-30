54a2cc3b1bb70cb690f9003d078bec613caa5218
wneves <drwilliamneves@gmail.com>
2023-10-30 14:53:45 -0400

    new page title style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image"
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h2>Github Workflow Test</h2>
+      <h2
+        className="text-4xl font-bold text-center text-gray-100 underline underline-offset-2"
+      >Github Workflow Test</h2>
 		</main>
 	)
 }
