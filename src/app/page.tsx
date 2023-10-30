b962def2382a72a0330798e03e1edf890a294340
wneves <drwilliamneves@gmail.com>
2023-10-30 15:00:53 -0400

    sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image"
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h2>Github Workflow Test</h2>
+      <h2
+        className="text-gray-200 text-center mb-0"
+      >Github Workflow Test</h2>
 		</main>
 	)
 }
